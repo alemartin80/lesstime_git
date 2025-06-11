c75cf61c3322d49bcc37c1bb7d7025d532616aec
alemartin80 <alemartin80@gmail.com>
2025-06-11 09:00:33 +0200

    commit

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,6 +5,7 @@ export * from './interfaces/producto-opcion.interface';
 export * from './interfaces/producto-opcion-opcion.interface';
 export * from './interfaces/categoria.interface';
 export * from './interfaces/utensilio-detalle.interface';
+export * from './interfaces/alergeno.interface';
 export * from './helpers/productos.helper';
 export * from './helpers/texto.helper';
 export * from './helpers/firebase.helper';
